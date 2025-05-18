9bf2ad0e3e7ca27e2624c7823e18fb69f33cefc9
Jatin Sharma <js0414030@gmail.com>
2025-05-18 10:41:06 +0530

    Modified

--- abhaya-news-backend/server.js
+++ abhaya-news-backend/server.js
@@ -3,12 +3,18 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const multer = require('multer');
+const path = require('path');
+
 
 // Load environment variables first
 dotenv.config();
 
 // Initialize Express app
 const app = express();
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public'))); // means: "if someone visits /news.html, serve the file from public/news.html."
+
 
 // Middleware
 app.use(cors());
@@ -36,11 +42,18 @@ const newsRoutes = require('./routes/newsRoutes');
 app.use('/api/admin', adminRoutes);
 app.use('/api/news', newsRoutes);
 
-// Basic route
+// // Basic route
+// app.get('/', (req, res) => {
+//   res.send('Welcome to ABHAYA News Backend!');
+// });
+
 app.get('/', (req, res) => {
-    res.send('Welcome to ABHAYA News Backend!');
+  res.sendFile(path.join(__dirname, 'public', 'news.html'));
 });
 
 // Server setup
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port http://localhost:5000`));
+
+
+
