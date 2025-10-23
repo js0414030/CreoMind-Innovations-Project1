85abf9d418e2aa717a55f229043aad14780f3020
Jatin Sharma <js0414030@gmail.com>
2025-10-24 01:11:55 +0530

    config updates

--- abhaya-news-frontend/config.js
+++ abhaya-news-frontend/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = `https://creomind-innovations.onrender.com/api`;
+const API_BASE_URL = `https://backend-abhaya-news-production.up.railway.app/api`;
 
 export default API_BASE_URL;
