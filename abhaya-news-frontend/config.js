497f14b2cf01dbba846783f7e6939477907ec3fe
Jatin Sharma <js0414030@gmail.com>
2025-11-06 23:08:08 +0530

    changed backend url

--- abhaya-news-frontend/config.js
+++ abhaya-news-frontend/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = `https://backend-abhaya-news-production.up.railway.app/api`;
+// const API_BASE_URL = `https://backend-abhaya-news-production.up.railway.app/api`;
+const API_BASE_URL = `https://creomind-innovations.onrender.com/api`;
 
 export default API_BASE_URL;
